351035f2f16ff83ee6bcf2bd207231c417d7cf8b
DDJager <danny-1995@live.nl>
2016-01-23 02:10:57 +0100

    Toyed around with the Http package and added some documentation to the Bootstrap.php file

--- src/Bootstrap.php
+++ src/Bootstrap.php
@@ -36,9 +36,21 @@ $whoops->register();
 $request = new \Http\HttpRequest($_GET, $_POST, $_COOKIE, $_FILES, $_SERVER); // Sets the $request object
 $response = new \Http\HttpResponse;	// Sets the $response object
 
+$cookieBuilder = new \Http\CookieBuilder;	// Sets the CookieBuilder object
+$cookieBuilder->setDefaultSecure(false);	// Disable the secure flag because this is only an example
+
+
+/**
+* Call the build function from the $cookiebuilder object with a key/value parameter the $cookieBuilder then returns
+* an HttpCookie $object which implements the Cookie interface we can then use all objects which
+* implements the Cookie interface as an object in the $response->addCookie() method
+*/
+// $cookie = $cookieBuilder->build('Key', 'Value');
+// $response->addCookie($cookie);
+
 foreach ($response->getHeaders() as $header) {
-    header($header, false);
+    header($header);
 }
 
-$response->setContent("ff");
-echo $response->getContent();
+
+
